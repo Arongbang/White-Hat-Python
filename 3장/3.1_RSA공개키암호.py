86c0112123868fabcd4a267835143032a0732dec
Arongbang <43198196+Arongbang@users.noreply.github.com>
2025-12-02 16:49:14 +0900

    3.1 주석 추가

--- 3장/3.1_RSA공개키암호.py
+++ 3장/3.1_RSA공개키암호.py
@@ -1,3 +1,5 @@
+#PKCS : 공개키 사용방식에 대한 표준 프로토콜
+#OAEP : 암호화 전 랜덤값을 추가하여 RSA 암호화를 수행
 from Cryptodome.Cipher import PKCS1_OAEP
 from Cryptodome.PublicKey import RSA
 
@@ -7,6 +9,7 @@ def rsa_enc(msg):
     public_key = private_key.publickey()
 
     # 공개키로 암호화
+	#공개키의 PKCS1_OEAP 객체 cipher를 생성
     cipher = PKCS1_OAEP.new(public_key)
     encdata = cipher.encrypt(msg)
     print("Encrypted:", encdata)
