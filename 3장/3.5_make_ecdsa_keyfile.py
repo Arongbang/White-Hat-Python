9a9258c67b6165cea36cdeed9ff2d65f07444090
Arongbang <43198196+Arongbang@users.noreply.github.com>
2025-12-03 15:23:03 +0900

    3.5 주석 추가

--- 3장/3.5_make_ecdsa_keyfile.py
+++ 3장/3.5_make_ecdsa_keyfile.py
@@ -2,6 +2,8 @@
 from Cryptodome.PublicKey import ECC
 
 def createPEM_ECDSA():
+	#ECC.generate() : 타원 곡선 암호를 이용해 개인키를 생성
+	#P-256 : 타원 곡선 암호를 만들기 위한 상수들 중 하나 (NIST에서 ’P-256'을 권장)
 	key = ECC.generate(curve='P-256')
 	with open ('privkey_eccdsa.pem', 'w') as h:
 		h.write(key.export_key(format='PEM'))
