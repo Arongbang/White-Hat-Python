c99d732cbec9a53d3fb22e1ea97b468f815297c2
Arongbang <43198196+Arongbang@users.noreply.github.com>
2025-12-03 14:55:11 +0900

    3.4 소스코드 업로드

---
+++ 3장/3.4_rsa_sign.py
@@ -0,0 +1,37 @@
+from Cryptodome.Signature import pkcs1_15
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Hash import SHA256 as SHA
+
+def readPEM(pemfile):
+	h = open(pemfile, 'r')
+	key = RSA.importKey(h.read())
+	h.close()
+	return key
+
+def rsa_sign(msg):
+	private_key = readPEM('privatekey.pem')
+	public_key = private_key.publicKey()
+	h = SHA.new(msg)
+	signature = pkcs1_15.new(private_key).sign(h)
+	return public_key, signature
+
+def rsa_verify(msg, public_key, signature):
+	h = SHA.new(msg)
+	
+	try:
+		pkcs1_15.new(public_key).verify(h, signature)
+		print('Authentic')
+	except Exception e:
+		print(e)
+		print('Not Authentic')
+	
+
+def main():
+	msg = 'My name is samsjang'
+	
+	public_key, signature = rsa_sign(msg.encode('utf-8'))
+	
+	rsa_verify(msg.encode('utf-8'), public_key, signature)
+
+
+main()
