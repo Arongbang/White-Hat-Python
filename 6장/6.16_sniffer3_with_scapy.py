ef97ff4719bba374c0bf6eca5eb223860e2fc3a2
Arongbang <43198196+Arongbang@users.noreply.github.com>
2025-12-30 09:53:57 +0900

    6.16 소스코드 업로드

---
+++ 6장/6.16_sniffer3_with_scapy.py
@@ -0,0 +1,17 @@
+from scapy.all import *
+
+
+def showPacket(packet):
+	data = '%s' %(packet[TCP].payload)
+	
+	if 'user' in data.lower() or 'data' in data.lower():
+		print('+++[%s] : %s' %(packet[IP].dst, data))
+	
+			
+def main(filter):
+	sniff(filter=filter, prn=showPacket, count=0, store=0)
+	
+
+if __name__ == '__main__':
+	filter = 'tcp port 25 or tcp port 110 or port 143'
+	main(filter)
