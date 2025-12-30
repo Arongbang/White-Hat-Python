0b8afe17c906639e73bb84beaddd3d19c1a69b2f
Arongbang <43198196+Arongbang@users.noreply.github.com>
2025-12-30 10:01:47 +0900

    6.16 주석 추가

--- 6장/6.16_sniffer3_with_scapy.py
+++ 6장/6.16_sniffer3_with_scapy.py
@@ -1,3 +1,4 @@
+#'user'나 ‘pass'라는 단어가 있을 경우, 서버 IP와 TCP 메시지를 화면에 출력
 from scapy.all import *
 
 
@@ -13,5 +14,6 @@ def main(filter):
 	
 
 if __name__ == '__main__':
+	#프로토콜이 TCT이며 포트는25, 110, 143만 스니핑하도록 filter 인자 전달
 	filter = 'tcp port 25 or tcp port 110 or port 143'
 	main(filter)
